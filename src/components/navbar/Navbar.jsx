ce557d25e6beecc5da666205ece79fe2ce51b53c
thantzin1843 <thantzinwin1843@gmail.com>
2024-07-03 21:40:26 +0630

    deploy

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,19 +3,22 @@ import React, { useState } from 'react'
 import Link from 'next/link'
 import styles from './navbar.module.css'
 import ThemeBtn from '../ThemeBtn/ThemeBtn'
+import { usePathname, useRouter } from 'next/navigation'
 
 function Navbar() {
-    const [show , setShow ] = useState(false)
+    const [show , setShow ] = useState(false);
+    const [path,setPath] = useState('/')
+
   return (
     <div className='relative'>
         <div className='w-full  h-[75px] items-center flex justify-between'>
         <div>Logo</div>
         <div className='sm:hidden md:flex gap-10'>
-            <Link href="#home" className={styles.active}>Home</Link>
-            <Link href="#aboutPage">About</Link>
-            <Link href="#projects">Projects</Link>
-            <Link href="#skills">Skills</Link>
-            <Link href="#contact">Contact</Link>
+            <Link href="#home" onClick={()=>setPath('/')} className={path=='/' && styles.active}>Home</Link>
+            <Link href="#aboutPage" onClick={()=>setPath('/about')} className={path=='/about' && styles.active}>About</Link>
+            <Link href="#projects" onClick={()=>setPath('/projects')} className={path=='/projects' && styles.active}>Projects</Link>
+            <Link href="#skills" onClick={()=>setPath('/skills')} className={path=='/skills' && styles.active}>Skills</Link>
+            <Link href="#contact" onClick={()=>setPath('/contact')} className={path=='/contact' && styles.active}>Contact</Link>
         </div>
 
         <ThemeBtn/>
